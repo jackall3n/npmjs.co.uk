669ce19eabcb7ef5942783e0d79c81631f6387cf
Jack Allen <jackallensemail+git@gmail.com>
2022-05-16 10:39:52 +0100

    Upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {
-      colors: {
-        urban: '#1D2439',
-        npm: '#cb3837'
-      },
-      fontFamily: {
-        npm: "'Source Sans Pro', 'Lucida Grande', sans-serif",
-        urban: '"Lora", Georgia, Cambria, "Times New Roman", Times, serif'
-      }
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                urban: '#1D2439',
+                npm: '#cb3837'
+            },
+            fontFamily: {
+                npm: "'Source Sans Pro', 'Lucida Grande', sans-serif",
+                urban: '"Lora", Georgia, Cambria, "Times New Roman", Times, serif'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
